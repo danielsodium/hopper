79dda6f9ef40a7793e0f77905ef5eb4e6ccd49d8
Daniel Na <element11sodium@gmail.com>
2024-07-08 18:26:22 -0400

    removing hardcoded level positions

--- src/server/Level.lua
+++ src/server/Level.lua
@@ -6,16 +6,17 @@ local LogGen = require(game.ServerScriptService.Server.LogGen)
 local Level = {}
 Level.__index = Level
 
-function Level.new()
+function Level.new(x, y, z)
     local self = setmetatable({}, Level)
 
-    self.y = 20;
-	self.x = 100;
+    self.y = y;
+	self.x = x;
+	self.z = z;
 
     self:createTerrain()
 
 	for i=0,8 do
-		LogGen.new(3, 0, 6, 60+10*i);
+		LogGen.new(3, self.x, self.y + 6, self.z + 60+10*i);
 	end
 
     return self
@@ -25,21 +26,20 @@ function Level:createTerrain()
 
     -- Create the starting land platform
     local startPlatform = Instance.new("Part")
-    startPlatform.Size = Vector3.new(self.x, self.y, 100)
-    startPlatform.Position = Vector3.new(0, 0, 0)
+    startPlatform.Size = Vector3.new(100, 20, 100)
+    startPlatform.Position = Vector3.new(self.x, self.y, self.z)
     startPlatform.Anchored = true
     startPlatform.Parent = workspace
     
     -- Create the ending land platform
     local endPlatform = Instance.new("Part")
-    endPlatform.Size = Vector3.new(self.x, self.y, 100)
-    endPlatform.Position = Vector3.new(0, 0, 200)
+    endPlatform.Size = Vector3.new(100, 20, 100)
+    endPlatform.Position = Vector3.new(self.x, self.y, self.z + 200)
     endPlatform.Anchored = true
     endPlatform.Parent = workspace
 
 	-- Create Death Water
-	local water = Water.new(0, -2, 100, 100, 15, 100);
-
+	local water = Water.new(self.x, self.y - 2, self.z + 100, 100, 15, 100);
 
 end
 
